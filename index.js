baf5a97c597ef14f639a05fe320196ad1bc6ee7f
Honel Guerrero <honelguerrero09@gmail.com>
2020-06-26 21:13:17 -0500

    Portafolio con correcciones

---
+++ index.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    $('.wrapper').addClass('loaded');
+    
+  }, 3000);
+});
+
+
+
+
+
+
+  /* Demo purposes only */
+  $(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
+
+
+
+ /* Demo purposes only */
+  $("figure").mouseleave(
+    function() {
+      $(this).removeClass("hover");
+    }
+  );
+s
+
+
+
+
